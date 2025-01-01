623286f81ea5d63d9a3936d0b5eb79af623ee2ce
ButhenaZw <bouthena762002@gmail.com>
2025-01-01 08:54:45 +0300

    commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// /////////Task 1/////////
+// let i=1;
+// for (let i=1; i<=10;){
+//     console.log(i);
+//     i++;
+// }
+// /////////Task 2/////////
+// while (i<=10){
+//     console.log(i);
+//     i++;
+// }
+// /////////Task 3/////////
+// let num = [1,2,3,4,5];
+// for (let i=0; i<num.length; i++){
+//     console.log(num[i]);
+// }
+/////////Task 4/////////
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+/////////Task 5/////////
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+///////Task 6/////////
+// let sum1 = [1, 2, 3, 4, 5];
+// console.log(sum1[sum1.length - 1]);
+///////Task 7/////////
